12478f806f32eafbb996d703790dc8163dcf0601
Jacqueline <jacquigavi921@gmail.com>
2024-03-09 23:08:16 -0500

    se modifica los externalServices y se modifica EmpresaRestService

--- oclientes/src/main/java/com/banquito/core/banking/service/ExternalServices/EmpresasRestService.java
+++ oclientes/src/main/java/com/banquito/core/banking/service/ExternalServices/EmpresasRestService.java
@@ -2,25 +2,46 @@ package com.banquito.core.banking.service.ExternalServices;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class EmpresasRestService {
 
-    private final RestTemplate restTemplate;
 
-    public ResponseEntity<String> obtenerEmpresas(){
-        String url = "http://localhost:8082/api/v1/empresas";
+    private final RestTemplate restTemplate; 
 
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new RuntimeException("Error al obtener la información del producto desde el servicio externo");
-        }
-        return response;
+    public EmpresasRestService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
     
+    public ResponseEntity<String> listarEmpresas() {
+        String url = "http://localhost:8081/api/v1/empresas";
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> obtenerPorId(String id) {
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/v1/empresas")
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUriString();
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> obtenerPorIdentificacion(String numeroIdentificacion) {
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/v1/empresas")
+                .path("/ruc/{numeroIdentificacion}")
+                .buildAndExpand(numeroIdentificacion)
+                .toUriString();
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> desactivar(String idCliente) {
+        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/v1/empresas")
+                .path("/desactivar/{idCliente}")
+                .buildAndExpand(idCliente)
+                .toUriString();
+        restTemplate.put(url, null);
+        return ResponseEntity.noContent().build();
+    }
 }
--- oclientes/src/main/java/com/banquito/core/banking/service/OEmpresaService.java
+++ oclientes/src/main/java/com/banquito/core/banking/service/OEmpresaService.java
@@ -2,73 +2,23 @@ package com.banquito.core.banking.service;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
 
 import com.banquito.core.banking.service.ExternalServices.EmpresasRestService;
 
 @Service
-public class EmpresaRestService {
+public class OEmpresaService {
 
-    private final RestTemplate restTemplate; 
-
-    public EmpresaRestService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public ResponseEntity<String> listarEmEntity() {
-        String url = "http://localhost:8081/api/v1/empresas/";
-        return restTemplate.getForEntity(url, String.class);
-    }
-
-    public ResponseEntity<String> obtenerPorTipoIdentificacionYNumero(String tipo, String numero) {
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/v1/clientes/")
-                .pathSegment(tipo, numero)
-                .build()
-                .toString();
-        return restTemplate.getForEntity(url, String.class);
-    }
-
-    public ResponseEntity<String> obtenerPorId(String id) {
-        String url = UriComponentsBuilder.fromHttpUrl("http://localhost:8081/api/v1/clientes/")
-                .path("/{id}")
-                .buildAndExpand(id)
-                .toUriString();
-        return restTemplate.getForEntity(url, String.class);
-    }
+    private final EmpresasRestService empresasRestService;
 
 
-
-    public EmpresaRestService(EmpresasRestService empresasRestService) {
+    public OEmpresaService(EmpresasRestService empresasRestService) {
         this.empresasRestService = empresasRestService;
     }
 
-    public ResponseEntity<String> listarTodo() {
-        return empresasRestService.obtenerEmpresas();
-    }
 
-    public ResponseEntity<String> obtenerPorId(String id) {
-        return empresasRestService.obtenerEmpresaPorId(id);
-    }
-
-    public ResponseEntity<String> obtenerPorIdentificacion(String numeroIdentificacion) {
-        return empresasRestService.obtenerEmpresaPorRUC(numeroIdentificacion);
-    }
-
-    public ResponseEntity<String> crear(Empresa empresa) {
-        return empresasRestService.crearEmpresa(empresa);
-    }
-
-    public ResponseEntity<String> actualizar(Empresa empresa) {
-        return empresasRestService.actualizarEmpresa(empresa);
-    }
-
-    public ResponseEntity<String> quitarMiembroEmpresa(String idEmpresa, String idMiembro) {
-        return empresasRestService.quitarMiembroDeEmpresa(idEmpresa, idMiembro);
-    }
-
-    public ResponseEntity<String> desactivar(String idEmpresa) {
-        return empresasRestService.desactivarEmpresa(idEmpresa);
+    public ResponseEntity<String> prueba(){
+        return this.empresasRestService.listarEmpresas();
     }
 
 }
